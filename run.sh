3706850140d0be556146a70d33a323d716b5c8d9
Kevin-XiongC <kevin_xiong1997@outlook.com>
2025-10-13 08:57:46 +0000

    fix log

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ NCCL_SOCKET_IFNAME=eth0 NCCL_IB_DISABLE=0 NCCL_IB_GID_INDEX=3 \
 NCCL_IB_HCA=mlx5_1:1,mlx5_2:1,mlx5_3:1,mlx5_4:1 \
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" TORCH_CUDA_ARCH_LIST=9.0 \
     python3 -m sglang.launch_server \
-    --model-path /root/llama --served-model-name llama --trust-remote-code \
+    --model-path /root/Llama-2-70b-hf --served-model-name llama --trust-remote-code \
     --host 0.0.0.0 --watchdog-timeout 1000000 \
     --tokenizer-worker-num 4 --disaggregation-mode prefill \
     --disaggregation-ib-device mlx5_1,mlx5_2,mlx5_3,mlx5_4 \
-    --context-length 131072 --max-prefill-tokens 32000 --chunked-prefill-size 32128 \
+    --context-length 4096 --max-prefill-tokens 32000 --chunked-prefill-size 32128 \
     --disable-radix-cache --tp-size 8 \
     --page-size 64 --max-running-requests 1024 --mem-fraction-static 0.90 \
     --load-balance-method round_robin  --load-format dummy 
@@ -23,7 +23,7 @@ NCCL_SOCKET_IFNAME=eth0 NCCL_IB_DISABLE=0 NCCL_IB_GID_INDEX=3 \
 NCCL_IB_HCA=mlx5_1:1,mlx5_2:1,mlx5_3:1,mlx5_4:1 \
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" TORCH_CUDA_ARCH_LIST=9.0 \
     python3 -m sglang.launch_server \
-    --model-path /root/llama --served-model-name llama \
+    --model-path /root/Llama-2-70b-hf --served-model-name llama \
     --trust-remote-code --tokenizer-worker-num 4 \
     --disaggregation-mode decode --disaggregation-ib-device mlx5_1,mlx5_2,mlx5_3,mlx5_4 \
     --disable-radix-cache --enable-dp-lm-head --enable-dp-attention \
